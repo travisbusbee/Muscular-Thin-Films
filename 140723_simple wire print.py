5996446943a4f0ebf8c4d0a54bc19b418aac5267
Jack Minardi <jack@minardi.org>
2014-07-30 14:42:00 -0400

    changed to work for minicaster

--- 140723_simple wire print.py
+++ 140723_simple wire print.py
@@ -2,13 +2,17 @@ from mecode import G
 import numpy as np
 
 # Cell allignment lines on 22mm slides
+outfile = "C:\Users\Wyss User\Documents\GitHub\Muscular-Thin-Films\lines.pgm"
+custom_header = "C:\Users\Wyss User\Documents\GitHub\Muscular-Thin-Films\minicaster_header.txt"
+custom_footer = "C:\Users\Wyss User\Documents\GitHub\Muscular-Thin-Films\minicaster_footer.txt"
 
 g = G(
     direct_write=False,
-    #outfile=outfile,
-    header=None,
-    footer=None,
-    #print_lines=False,
+    aerotech_include = False, #include robomama header and footer
+    outfile=outfile,
+    header=custom_header,
+    footer=custom_footer,
+    print_lines=False,
     )
 
 #building array
@@ -17,7 +21,7 @@ start_y = 0
 
 array_centerpoints = np.zeros((2,2,2))
 
-increment = 10
+increment = 14
 
 for i in range(0,2,1):
     for j in range(0,2,1):
@@ -30,24 +34,24 @@ for i in range(0,2,1):
 array_centerpoints = np.reshape(array_centerpoints,(4,2))
 
 #Somehow get surface center points
-array_centerpoints_surface = (0.0, 0.0, 0.0, 0.0)
+array_centerpoints_surface = (-2.35156,)*4
 
 
 #Settings
 
-pressure_box = 4
+pressure_box = 5
 
-area_width = 4
+area_width = 10
 
-area_length = 4
+area_length = 10
 
 wire_height=(0.005,0.010,0.025, 0.050)
 
-wire_pressure=(4.8,)*4
+wire_pressure=(8,)*4
 
 wire_speed=(4,)*4
 
-wire_spacing = 1
+wire_spacing = 2
 
 
                                                                                                 
@@ -57,7 +61,7 @@ def meander_tail(x, y, z, spacing, orientation, tail, speed, clip_direction, noz
     g.abs_move(**{nozzle:z})
     g.feed(1) 
     if valve is not None:
-        g.set_valve(num = valve, value = 1)
+        g.set_valve(num = valve, value = 1)    
     g.dwell(dwell)
     g.move(x=tail)
     g.feed(speed)
@@ -65,9 +69,10 @@ def meander_tail(x, y, z, spacing, orientation, tail, speed, clip_direction, noz
     g.move(x=tail)
     if valve is not None:
         g.set_valve(num = valve, value = 0) 
+    g.toggle_pressure(pressure_box)
     g.dwell(0.3)
     g.clip(axis=nozzle, direction=clip_direction, height=5)
-    
+
 
 
 def wire_all (nozzle, valve):
@@ -75,12 +80,16 @@ def wire_all (nozzle, valve):
     for i in range(0,4):
         g.feed(30)
         g.abs_move(*array_centerpoints[i])
-        g.move(x=-2,y=-2)
+        g.move(x=-5,y=-5)
         g.set_pressure(pressure_box, wire_pressure[i])
+        g.toggle_pressure(pressure_box)
         meander_tail (x=area_width, y=-area_length, z=(wire_height[i]+array_centerpoints_surface[i]), spacing=wire_spacing, orientation = 'y', tail = 1, speed=wire_speed[i], clip_direction = '+y', nozzle = nozzle, valve = valve, dwell = 0.75 )
+        
 
+g.toggle_pressure(5)
+#lets g
+#g.set_home(x=0, y=0)
+#wire_all(nozzle= 'Z', valve = 0)
+#g.write('POSOFFSET CLEAR X Y U Z')
 
-#lets go
-
-wire_all(nozzle= 'A', valve = 0)
-
+g.teardown()
